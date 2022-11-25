682333abd4cd170b02944208ff0649cae17cd5c9
Kidnxs <kidnxs@Kidnxss-MacBook-Pro.local>
2022-11-25 12:04:21 +0700

    hot fix pacekage.json

--- src/core/server.js
+++ src/core/server.js
@@ -47,7 +47,8 @@ router.render = (req, res) => {
 };
 
 // Use default router
+const PORT = process.env.PORT || 6969;
 server.use('/api', router);
-server.listen(6969, () => {
+server.listen(PORT, () => {
   console.log('JSON Server is running');
 });
